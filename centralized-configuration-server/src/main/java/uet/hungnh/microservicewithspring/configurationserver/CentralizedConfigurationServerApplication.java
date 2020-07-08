532cfa2a088398ac5844c75cd884c19fd808b8d3
Jon <hungnh.uet@gmail.com>
2020-07-08 21:40:30 +0700

    Add centralized configuration server

---
+++ centralized-configuration-server/src/main/java/uet/hungnh/microservicewithspring/configurationserver/CentralizedConfigurationServerApplication.java
@@ -0,0 +1,15 @@
+package uet.hungnh.microservicewithspring.configurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CentralizedConfigurationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralizedConfigurationServerApplication.class, args);
+    }
+
+}
---
+++ centralized-configuration-server/src/test/java/uet/hungnh/microservicewithspring/configurationserver/CentralizedConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uet.hungnh.microservicewithspring.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
