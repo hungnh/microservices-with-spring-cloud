2abf7c05090056479e1c44cab26913282cff78b2
Jon <hungnh.uet@gmail.com>
2020-07-30 22:19:05 +0700

    add word server

---
+++ lab05/lab05-word/src/main/java/uet/hungnh/microservicewithspring/lab05word/Lab05WordApplication.java
@@ -0,0 +1,13 @@
+package uet.hungnh.microservicewithspring.lab05word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab05WordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab05WordApplication.class, args);
+    }
+
+}
---
+++ lab05/lab05-word/src/test/java/uet/hungnh/microservicewithspring/lab05word/Lab05WordApplicationTests.java
@@ -0,0 +1,13 @@
+package uet.hungnh.microservicewithspring.lab05word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab05WordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
