84846a80f0d58ae54933aac8b0b5e1a6d209ecf4
Jon <hungnh.uet@gmail.com>
2020-10-02 22:26:11 +0700

    lab08 - spring cloud bus

---
+++ lab08/lab08-client/src/main/java/uet/hungnh/microservicewithspring/lab08client/Lab08ClientApplication.java
@@ -0,0 +1,13 @@
+package uet.hungnh.microservicewithspring.lab08client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab08ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab08ClientApplication.class, args);
+    }
+
+}
---
+++ lab08/lab08-client/src/test/java/uet/hungnh/microservicewithspring/lab08client/Lab08ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package uet.hungnh.microservicewithspring.lab08client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab08ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
